8fc25147d5ea6b4c4727c82535d8211421472943
Stephen Collins <swcollins462@gmail.com>
2023-09-10 21:26:31 -0400

    user login

--- manager_app/views.py
+++ manager_app/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .models import Player
 
 
 def home(request):
-    return render(request, 'manager_app/home.html')
+    # Check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been logged in!')
+            return redirect('home')
+        else:
+            messages.success(
+                request, 'There was an error logging in. Please try again.')
+            return redirect('home')
+    else:
+        return render(request, 'manager_app/home.html', {})
+
+
+def logout_user(request):
+    pass
