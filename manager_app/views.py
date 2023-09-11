2197a5a28926fed3be8b8b36566d842ae32ffac8
Stephen Collins <swcollins462@gmail.com>
2023-09-10 20:24:45 -0400

    navbar

--- manager_app/views.py
+++ manager_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Player
 
 
-def index(request):
-    return render(request, 'manager_app/template.html')
+def home(request):
+    return render(request, 'manager_app/home.html')
