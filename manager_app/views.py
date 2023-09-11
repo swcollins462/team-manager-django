8b18b33b4d784f65855f04c67b9785dd8ea3fc98
Stephen Collins <swcollins462@gmail.com>
2023-09-10 22:55:27 -0400

    user registration

--- manager_app/views.py
+++ manager_app/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .models import Player
+from .forms import SignUpForm
 
 
 def home(request):
+    context = {
+        'menu_item': 'home',
+    }
+
     # Check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -20,10 +24,34 @@ def home(request):
                 request, 'There was an error logging in. Please try again.')
             return redirect('home')
     else:
-        return render(request, 'manager_app/home.html', {})
+        return render(request, 'manager_app/home.html', context)
 
 
 def logout_user(request):
     logout(request)
     messages.success(request, 'You have been logged out.')
     return redirect('home')
+
+
+def register_user(request):
+    context = {
+        'page': 'Register',
+        'menu_item': 'register',
+    }
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(
+                request, 'You have successfully registered. Welcome!')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'manager_app/register.html', {**context, 'form': form})
+
+    return render(request, 'manager_app/register.html', {**context, 'form': form})
